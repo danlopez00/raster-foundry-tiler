4f70d03e16abc615ddcce5ee3eba60dcc9a046c5
Kevin DeLoach <kdeloach@azavea.com>
2015-10-16 13:54:00 -0400

    Create bootstrap requirement.txt temp file
    The purpose of this is to lock Python dependencies to specific version
    numbers.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,15 @@
 sudo yum-config-manager --enable epel
 sudo yum -y install geos proj proj-nad proj-epsg
 sudo ln -s /usr/lib64/libproj.so.0 /usr/lib64/libproj.so
+
 curl http://oam-server-tiler.s3.amazonaws.com/emr/gdal-1.11.2-amz1.tar.gz | sudo tar zxf - -C /usr/local
-sudo GDAL_CONFIG=/usr/local/bin/gdal-config pip-2.7 install boto3 rasterio mercantile psutil docopt
+
+cat << EOF > /tmp/requirements.txt
+boto3==1.1.4
+docopt==0.6.2
+mercantile==0.8.3
+psutil==3.2.2
+rasterio==0.28.0
+EOF
+
+sudo GDAL_CONFIG=/usr/local/bin/gdal-config pip-2.7 install -r /tmp/requirements.txt
